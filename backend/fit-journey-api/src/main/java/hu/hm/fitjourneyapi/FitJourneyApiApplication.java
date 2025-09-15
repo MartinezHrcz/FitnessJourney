bd97e6ed3061ba1c96571d313024f9883dfdaed4
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-15 22:05:40 +0200

    chore(api): dependencies added for future, .gitignore made

---
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/FitJourneyApiApplication.java
@@ -0,0 +1,13 @@
+package hu.hm.fitjourneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitJourneyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitJourneyApiApplication.class, args);
+    }
+
+}
---
+++ backend/fit-journey-api/src/test/java/hu/hm/fitjourneyapi/FitJourneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hm.fitjourneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitJourneyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
