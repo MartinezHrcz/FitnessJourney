768e7402c84582e6543705e9643c9f88e130289f
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-16 14:55:22 +0200

    feat(api):add docker

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/FitJourneyApiApplication.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/FitJourneyApiApplication.java
@@ -2,12 +2,18 @@ package hu.hm.fitjourneyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FitJourneyApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FitJourneyApiApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "hi";
+    }
 }
