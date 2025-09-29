0c4de6e5903359b116823fed3aa5fecb48572d5e
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-29 21:01:42 +0200

    feat(api): implemented MessageMapper DTO to entity and entity to DTO

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/dto/social/message/MessageDTO.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/dto/social/message/MessageDTO.java
@@ -1,5 +1,12 @@
 package hu.hm.fitjourneyapi.dto.social.message;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
 public class MessageDTO {
     private long senderId;
     private long recipientId;
--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/MessageMapper.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/MessageMapper.java
@@ -1,4 +1,29 @@
 package hu.hm.fitjourneyapi.mapper.social;
 
+import hu.hm.fitjourneyapi.dto.social.message.MessageDTO;
+import hu.hm.fitjourneyapi.model.User;
+import hu.hm.fitjourneyapi.model.social.Message;
+
 public class MessageMapper {
+
+    public static MessageDTO toDTO(Message message) {
+        if (message == null) return null;
+        return MessageDTO
+                .builder()
+                .recipientId(message.getRecipient().getId())
+                .senderId(message.getSender().getId())
+                .content(message.getContent())
+                .sentTime(message.getId())
+                .build();
+    }
+
+    public static Message toMessage(MessageDTO messageDTO,User sender, User recipient) {
+        if (messageDTO == null) return null;
+        return Message
+                .builder()
+                .sender(sender)
+                .recipient(recipient)
+                .content(messageDTO.getContent())
+                .build();
+    }
 }
