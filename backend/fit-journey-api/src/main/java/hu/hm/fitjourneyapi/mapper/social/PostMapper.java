3c56f882974c750ca9881eb64430176ac1ec27ac
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-30 18:39:35 +0200

    refact(api): post mapper refactored to Mapstruct

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/PostMapper.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/PostMapper.java
@@ -3,34 +3,27 @@ package hu.hm.fitjourneyapi.mapper.social;
 import hu.hm.fitjourneyapi.dto.social.post.PostDTO;
 import hu.hm.fitjourneyapi.model.User;
 import hu.hm.fitjourneyapi.model.social.Post;
+import jdk.jfr.Name;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class PostMapper {
-    public static PostDTO toPostDTO(Post post) {
-        if (post == null) return null;
-        return PostDTO
-                .builder()
-                .id(post.getId())
-                .userId(post.getUser().getId())
-                .title(post.getTitle())
-                .content(post.getContent())
-                .sentTime(post.getSentTime())
-                .build();
-    }
+@Mapper
+public interface PostMapper {
 
-    public static List<PostDTO> toListPostDTO(List<Post> posts) {
-        if (posts == null) return null;
-        return posts.stream().map(PostMapper::toPostDTO).collect(Collectors.toList());
-    }
+    @Mapping(source = "user", target = "userId", qualifiedByName = "userToId")
+    PostDTO toPostDTO(Post post);
 
-    public static Post toPost(PostDTO postDTO, User user) {
-        if (postDTO == null) return null;
-        return Post.builder()
-                .user(user)
-                .title(postDTO.getTitle())
-                .content(postDTO.getContent())
-                .build();
+    List<PostDTO> toListPostDTO(List<Post> posts);
+
+    @Mapping(target = "user", expression = "java(user)")
+    Post toPost(PostDTO postDTO, User user);
+
+    @Named("userToId")
+    static Long userToId(User user) {
+        return user != null ? user.getId() : null;
     }
 }
