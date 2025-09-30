abb459455f4d556f854c0eb818011f83ee380f44
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-30 19:04:48 +0200

    refact(api): FriendMapper refactored to Mapstruct

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/FriendMapper.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/FriendMapper.java
@@ -3,33 +3,28 @@ package hu.hm.fitjourneyapi.mapper.social;
 import hu.hm.fitjourneyapi.dto.social.friend.FriendDTO;
 import hu.hm.fitjourneyapi.model.User;
 import hu.hm.fitjourneyapi.model.social.Friend;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class FriendMapper {
-    public static FriendDTO toFriendDTO(Friend friend) {
-        if (friend == null) return null;
-        return FriendDTO.builder()
-                .id(friend.getId())
-                .userId(friend.getUser().getId())
-                .friendId(friend.getFriend().getId())
-                .status(friend.getStatus())
-                .requestedTime(friend.getRequestedTime())
-                .build();
-    }
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface FriendMapper {
+    @Mapping(source="user", target= "userId", qualifiedByName = "userToId")
+    @Mapping(source="friend", target= "friendId", qualifiedByName = "userToId")
+    FriendDTO toFriendDTO(Friend friend);
 
-    public static List<FriendDTO> toListFriendDTO(List<Friend> friends) {
-        if (friends == null) return null;
-        return friends.stream().map(FriendMapper::toFriendDTO).collect(Collectors.toList());
-    }
+    List<FriendDTO> toListFriendDTO(List<Friend> friends);
 
-    public static Friend toFriend(FriendDTO friendDTO, User user, User friend  ) {
-        return Friend.builder()
-                .user(user)
-                .friend(friend)
-                .status(friendDTO.getStatus())
-                .requestedTime(friendDTO.getRequestedTime())
-                .build();
+    @Mapping(target = "user", expression = "java(user)")
+    @Mapping(target = "friend", expression = "java(friend)")
+    Friend toFriend(FriendDTO friendDTO, User user, User friend  );
+
+    @Named("userToId")
+    static Long userToId(User user) {
+        return user != null ? user.getId() : null;
     }
 }
