15925c91b780183b603b063778b265490e86084e
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-30 18:12:13 +0200

    refact(api): user mapper refactored to Mapstruct

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/UserMapper.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/UserMapper.java
@@ -3,37 +3,17 @@ package hu.hm.fitjourneyapi.mapper;
 import hu.hm.fitjourneyapi.dto.user.UserCreateDTO;
 import hu.hm.fitjourneyapi.dto.user.UserDTO;
 import hu.hm.fitjourneyapi.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class UserMapper {
-    public static UserDTO toUserDTO(final User user) {
-        if (user == null) return null;
-        return UserDTO.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .email(user.getEmail())
-                .birthday(user.getBirthday())
-                .heightInCm(user.getHeightInCm())
-                .weightInKg(user.getWeightInKg())
-                .build();
-    }
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+    UserDTO toUserDTO(User user);
 
-    public static List<UserDTO> toUserDTOList(final List<User> users) {
-        if (users == null) return null;
-        return users.stream().map(UserMapper::toUserDTO).collect(Collectors.toList());
-    }
+    List<UserDTO> toUserDTOList(List<User> users);
 
-    public static User toUser(final UserCreateDTO userCreateDTO) {
-        if (userCreateDTO == null) return null;
-        return User.builder()
-                .name(userCreateDTO.getName())
-                .email(userCreateDTO.getEmail())
-                .password(userCreateDTO.getPassword())
-                .heightInCm(userCreateDTO.getHeightInCm())
-                .weightInKg(userCreateDTO.getWeightInKg())
-                .birthday(userCreateDTO.getBirthday())
-                .build();
-    }
+    User toUser(UserCreateDTO userCreateDTO);
 }
