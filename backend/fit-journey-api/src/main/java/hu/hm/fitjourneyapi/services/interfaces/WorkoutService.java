0e2a306ddaae576ac373621192f4f4a7a8e9f92d
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-01 12:04:45 +0200

    feat(api): add workoutService interface

---
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/interfaces/WorkoutService.java
@@ -0,0 +1,19 @@
+package hu.hm.fitjourneyapi.services.interfaces;
+
+import hu.hm.fitjourneyapi.dto.fitness.workout.WorkoutCreateDTO;
+import hu.hm.fitjourneyapi.dto.fitness.workout.WorkoutDTO;
+import hu.hm.fitjourneyapi.model.fitness.Workout;
+
+import java.util.List;
+
+public interface WorkoutService {
+    WorkoutDTO createWorkout(WorkoutCreateDTO workoutCreateDTO);
+
+    WorkoutDTO getWorkoutByWorkoutId(long id);
+
+    List<WorkoutDTO> getWorkouts();
+
+    List<WorkoutDTO> getWorkoutByUserId(long id);
+
+    void deleteWorkoutById(long id);
+}
