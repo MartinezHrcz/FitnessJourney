b33a3520fe361204b30287c22f63a738a06a3ff6
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-01 12:09:47 +0200

    feat(api): add ExerciseService interface

---
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/interfaces/ExerciseService.java
@@ -0,0 +1,34 @@
+package hu.hm.fitjourneyapi.services.interfaces;
+
+import hu.hm.fitjourneyapi.dto.fitness.excercise.AbstractExerciseDTO;
+import hu.hm.fitjourneyapi.dto.fitness.excercise.ExerciseCardioSetDTO;
+import hu.hm.fitjourneyapi.dto.fitness.excercise.ExerciseFlexibilitySetDTO;
+import hu.hm.fitjourneyapi.dto.fitness.excercise.ExerciseStrengthSetDTO;
+
+import java.util.List;
+
+public interface ExerciseService {
+    List<AbstractExerciseDTO> getExercises();
+
+    List<AbstractExerciseDTO> getByWorkoutId(long id);
+
+    List<AbstractExerciseDTO> getByUserId(long id);
+
+    List<AbstractExerciseDTO> getByName(String name);
+
+    AbstractExerciseDTO getById(long id);
+
+    ExerciseStrengthSetDTO createExerciseStrengthSet(ExerciseStrengthSetDTO exerciseStrengthSetDTO);
+
+    ExerciseFlexibilitySetDTO createExerciseFlexibilitySet(ExerciseFlexibilitySetDTO flexibilitySetDTO);
+
+    ExerciseCardioSetDTO createExerciseCardioSet(ExerciseCardioSetDTO cardioSetDTO);
+
+    ExerciseStrengthSetDTO updateExerciseStrengthSet(ExerciseStrengthSetDTO dto);
+
+    ExerciseFlexibilitySetDTO updateExerciseFlexibilitySet(ExerciseFlexibilitySetDTO dto);
+
+    ExerciseCardioSetDTO updateExerciseCardioSet(ExerciseCardioSetDTO dto);
+
+    void deleteExerciseById(long id);
+}
