08ab4176e089febc74d03bdd4b5bb7d998f4efbd
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-01 12:26:34 +0200

    feat(api): add search by name to FriendService

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/interfaces/FriendService.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/interfaces/FriendService.java
@@ -8,6 +8,7 @@ public interface FriendService {
     FriendDTO getFriendById(long id);
     List<FriendDTO> getFriends();
     List<FriendDTO> getFriendsByUserId(long id);
+    List<FriendDTO> getFriendsByUserIdAndRecipientName(long id, String recipientName);
     FriendDTO updateFriend(FriendDTO friendDTO);
     FriendDTO createFriend(FriendDTO friendDTO);
     void deleteFriend(long id);
