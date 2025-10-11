c7955ac43dc70bd6e6b17b67625ff7564fb6cc1c
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-11 20:30:11 +0200

    feat(api): implemented create into PostServiceImpl

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/implementation/social/PostServiceImpl.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/implementation/social/PostServiceImpl.java
@@ -5,7 +5,9 @@ import hu.hm.fitjourneyapi.dto.social.post.PostDTO;
 import hu.hm.fitjourneyapi.dto.social.post.PostUpdateDTO;
 import hu.hm.fitjourneyapi.exception.social.post.PostNotFoundException;
 import hu.hm.fitjourneyapi.mapper.social.PostMapper;
+import hu.hm.fitjourneyapi.model.User;
 import hu.hm.fitjourneyapi.model.social.Post;
+import hu.hm.fitjourneyapi.repository.UserRepository;
 import hu.hm.fitjourneyapi.repository.social.PostRepository;
 import hu.hm.fitjourneyapi.services.interfaces.social.PostService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +21,13 @@ public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
     private final PostMapper postMapper;
+    private final UserRepository userRepository;
 
 
-    public PostServiceImpl(PostRepository postRepository, PostMapper postMapper) {
+    public PostServiceImpl(PostRepository postRepository, PostMapper postMapper, UserRepository userRepository) {
         this.postRepository = postRepository;
         this.postMapper = postMapper;
+        this.userRepository = userRepository;
     }
 
 
@@ -76,7 +80,21 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDTO createPost(PostCreateDTO postCreateDTO) {
-        return null;
+        log.debug("Attempting to create post");
+        User user =userRepository.findById(postCreateDTO.getUserId()).orElseThrow(
+                ()-> {
+                    log.warn("User not found with id: {}", postCreateDTO.getUserId());
+                    return new PostNotFoundException("User not found with id: " + postCreateDTO.getUserId());
+                }
+        );
+        Post post = Post.builder()
+                .user(user)
+                .title(postCreateDTO.getTitle())
+                .content(postCreateDTO.getContent())
+                .build();
+        log.info("Created post with id: {} ",postCreateDTO.getUserId());
+        postRepository.save(post);
+        return postMapper.toPostDTO(post);
     }
 
     @Override
