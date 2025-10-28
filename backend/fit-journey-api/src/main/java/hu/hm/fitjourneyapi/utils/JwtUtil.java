796fca4da4b50d51d28782657695708714051ef1
Herczeg Martinez <herczegmartinez@gmail.com>
2025-10-28 20:10:59 +0100

    feat: add extract username method to JwtUtil

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/utils/JwtUtil.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/utils/JwtUtil.java
@@ -1,13 +1,16 @@
 package hu.hm.fitjourneyapi.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -31,6 +34,23 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
 }
