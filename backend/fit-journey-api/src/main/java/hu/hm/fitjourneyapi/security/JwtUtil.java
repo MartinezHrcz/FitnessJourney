c2f063aefa94639f1cfe9778b36cf1be1b8fc4db
Herczeg Martinez <herczegmartinez@gmail.com>
2025-10-28 20:18:49 +0100

    refact: make new package named security for jwt

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/security/JwtUtil.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/security/JwtUtil.java
@@ -1,9 +1,8 @@
-package hu.hm.fitjourneyapi.utils;
+package hu.hm.fitjourneyapi.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
