2aa5aeaaa47b7d8f6d73dad2539d42386a33a14d
Herczeg Martinez <herczegmartinez@gmail.com>
2025-10-29 11:24:40 +0100

    chore: add empty JwtAuthenticationFilter

---
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/security/JwtAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package hu.hm.fitjourneyapi.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(UserDetailsService userDetailsService, JwtUtil jwtUtil) {
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
