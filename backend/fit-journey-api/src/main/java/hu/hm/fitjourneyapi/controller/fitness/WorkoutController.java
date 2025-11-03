3178b9b0ddd2a3aba27a1a1a2144dad7b5bdb1e9
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-03 19:53:56 +0100

    feat: add createWorkout to WorkoutController

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/fitness/WorkoutController.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/fitness/WorkoutController.java
@@ -1,4 +1,35 @@
 package hu.hm.fitjourneyapi.controller.fitness;
 
+import hu.hm.fitjourneyapi.dto.fitness.workout.WorkoutCreateDTO;
+import hu.hm.fitjourneyapi.dto.fitness.workout.WorkoutDTO;
+import hu.hm.fitjourneyapi.exception.userExceptions.UserNotFound;
+import hu.hm.fitjourneyapi.services.interfaces.fitness.WorkoutService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/workout")
 public class WorkoutController {
+    WorkoutService workoutService;
+
+    public WorkoutController(WorkoutService workoutService) {
+        this.workoutService = workoutService;
+    }
+
+    @PostMapping
+    public ResponseEntity<WorkoutDTO> createWorkout(@RequestBody WorkoutCreateDTO  workoutCreateDTO) {
+        try {
+            return ResponseEntity.ok(workoutService.createWorkout(workoutCreateDTO));
+        }
+        catch (UserNotFound ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
