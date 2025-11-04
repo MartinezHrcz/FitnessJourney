d76901fc9004f1559e5aadf0bd2cbfe12238621d
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-04 20:18:35 +0100

    chore: empty ExerciseController with base request mapping and dependency injection

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/fitness/ExerciseController.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/fitness/ExerciseController.java
@@ -1,4 +1,18 @@
 package hu.hm.fitjourneyapi.controller.fitness;
 
+import hu.hm.fitjourneyapi.services.interfaces.fitness.ExerciseService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exercise")
 public class ExerciseController {
+
+    ExerciseService exerciseService;
+
+    public ExerciseController(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+
 }
