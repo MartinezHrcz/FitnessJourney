af1d3e26ca11e551208f8df1ffc08aea522d21d3
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-05 19:48:30 +0100

    feat: all get mappings for exercises have been made, + delete mapping

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/fitness/ExerciseController.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/fitness/ExerciseController.java
@@ -1,12 +1,13 @@
 package hu.hm.fitjourneyapi.controller.fitness;
 
 import hu.hm.fitjourneyapi.dto.fitness.excercise.AbstractExerciseDTO;
+import hu.hm.fitjourneyapi.exception.fitness.ExerciseNotFound;
 import hu.hm.fitjourneyapi.services.interfaces.fitness.ExerciseService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/exercise")
@@ -27,4 +28,41 @@ public class ExerciseController {
             return ResponseEntity.badRequest().build();
         }
     }
+
+    @GetMapping
+    public ResponseEntity<List<AbstractExerciseDTO>> getExercises() {
+        return ResponseEntity.ok(exerciseService.getExercises());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AbstractExerciseDTO> getExercise(@PathVariable long id) {
+        try{
+            return ResponseEntity.ok(exerciseService.getById(id));
+        }
+        catch (ExerciseNotFound ex)
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/byname/{name}")
+    public ResponseEntity<List<AbstractExerciseDTO>> getExercisesByName(@PathVariable String name) {
+        return ResponseEntity.ok(exerciseService.getByName(name));
+    }
+
+    @GetMapping("/byuser/{id}")
+    public ResponseEntity<List<AbstractExerciseDTO>> getExercisesByUser(@PathVariable UUID id) {
+        return ResponseEntity.ok(exerciseService.getByUserId(id));
+    }
+
+    @GetMapping("/byworkout/{id}")
+    public ResponseEntity<List<AbstractExerciseDTO>> getExercisesByWorkout(@PathVariable long id) {
+        return ResponseEntity.ok(exerciseService.getByWorkoutId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteExercise(@PathVariable long id) {
+        return ResponseEntity.noContent().build();
+    }
+
 }
