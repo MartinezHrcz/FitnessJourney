83dc27ba47241c0489dbe540dd27a749d3af37f7
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-09 19:32:25 +0100

    feat: add UserMadeExercisesMapper for dto -> entity, entity -> dto conversions

---
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/fitness/UserMadeExercisesMapper.java
@@ -0,0 +1,20 @@
+package hu.hm.fitjourneyapi.mapper.fitness;
+
+import hu.hm.fitjourneyapi.dto.fitness.exerciseTemplates.UserMadeExercisesDTO;
+import hu.hm.fitjourneyapi.model.fitness.UserMadeTemplates;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMadeExercisesMapper {
+
+    UserMadeTemplates toEntity(UserMadeExercisesDTO dto);
+
+    UserMadeExercisesDTO toDto(UserMadeTemplates entity);
+
+    List<UserMadeExercisesDTO> toDto(List<UserMadeTemplates> entity);
+
+    List<UserMadeTemplates> toEntity(List<UserMadeExercisesDTO> dto);
+}
