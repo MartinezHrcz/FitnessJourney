e326f318ca5d0cfec69b248b978b61ff1d5eb669
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-10 10:36:53 +0100

    feat: get methods implemented to UserExerciseServiceImpl

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/repository/fitness/UserMadeTemplateRepository.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/repository/fitness/UserMadeTemplateRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface UserMadeTemplateRepository extends JpaRepository<UserMadeTemplates, UUID> {
+public interface UserMadeTemplateRepository extends JpaRepository<UserMadeTemplates, Long> {
     List<UserMadeTemplates> findAllByNameContainingIgnoreCase(String name);
     boolean existsByNameIgnoreCase(String name);
 }
--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/implementation/fitness/exerciseTemplates/UserExerciseServiceImpl.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/implementation/fitness/exerciseTemplates/UserExerciseServiceImpl.java
@@ -2,28 +2,42 @@ package hu.hm.fitjourneyapi.services.implementation.fitness.exerciseTemplates;
 
 import hu.hm.fitjourneyapi.dto.fitness.exerciseTemplates.UserExerciseUpdateDto;
 import hu.hm.fitjourneyapi.dto.fitness.exerciseTemplates.UserMadeExercisesDTO;
+import hu.hm.fitjourneyapi.exception.fitness.ExerciseNotFound;
+import hu.hm.fitjourneyapi.mapper.fitness.UserMadeExercisesMapper;
+import hu.hm.fitjourneyapi.model.fitness.UserMadeTemplates;
+import hu.hm.fitjourneyapi.repository.fitness.UserMadeTemplateRepository;
 import hu.hm.fitjourneyapi.services.interfaces.fitness.UserExerciseService;
 
 import java.util.List;
 
 public class UserExerciseServiceImpl implements UserExerciseService {
 
-    private final UserMadeTemplatesRepository
+    private final UserMadeTemplateRepository repository;
+    private final UserMadeExercisesMapper mapper;
+
+    public UserExerciseServiceImpl(UserMadeTemplateRepository repository, UserMadeExercisesMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
 
     @Override
     public List<UserMadeExercisesDTO> getUserMadeExercises(long id) {
-        return List.of();
+        return mapper.toDto(repository.findAll());
     }
 
     @Override
     public List<UserMadeExercisesDTO> getUserMadeExercisesByName(String name) {
-        return List.of();
+        return mapper.toDto(repository.findAllByNameContainingIgnoreCase(name));
     }
 
     @Override
     public UserMadeExercisesDTO getUserMadeExercise(long id) {
-        return null;
+        UserMadeTemplates template = repository.findById(id).orElseThrow(
+                () -> new ExerciseNotFound("Exercise not found by id: " + id)
+        );
+
+        return mapper.toDto(template);
     }
 
     @Override
