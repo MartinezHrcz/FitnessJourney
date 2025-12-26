65ce3b9106fb8db9f946cd68aea276a36d4aa299
Herczeg Martinez <herczegmartinez@gmail.com>
2025-12-26 18:04:18 +0100

    feat: implement post controller

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/social/PostController.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/controller/social/PostController.java
@@ -1,4 +1,77 @@
 package hu.hm.fitjourneyapi.controller.social;
 
+import hu.hm.fitjourneyapi.dto.social.post.PostCreateDTO;
+import hu.hm.fitjourneyapi.dto.social.post.PostDTO;
+import hu.hm.fitjourneyapi.dto.social.post.PostUpdateDTO;
+import hu.hm.fitjourneyapi.services.interfaces.social.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/post")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostDTO>> getAll() {
+        return ResponseEntity.ok(postService.getPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getById(@PathVariable UUID id) {
+        try {
+          return ResponseEntity.ok(postService.getPostById(id));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<PostDTO>> getByUserId(@PathVariable UUID id) {
+        try {
+          return ResponseEntity.ok(postService.getPostsByUserId(id));
+        }
+        catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDTO> create(@RequestBody PostCreateDTO createDTO) {
+        try {
+            return ResponseEntity.ok(postService.createPost(createDTO));
+        }
+        catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDTO> update(@PathVariable UUID id, @RequestBody PostUpdateDTO updateDTO) {
+        try{
+            return ResponseEntity.ok(postService.updatePost(id, updateDTO));
+        }
+        catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable UUID id) {
+        try{
+            postService.deletePostById(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
