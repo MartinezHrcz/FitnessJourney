2eb53e39760b4e0d5a63e2973e8ec49517e8835c
Herczeg Martinez <herczegmartinez@gmail.com>
2026-01-11 19:10:01 +0100

    cleanup

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/FitJourneyApiApplication.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/FitJourneyApiApplication.java
@@ -1,17 +1,9 @@
 package hu.hm.fitjourneyapi;
 
-import hu.hm.fitjourneyapi.dto.fitness.workout.WorkoutDTO;
-import hu.hm.fitjourneyapi.mapper.fitness.WorkoutMapper;
-import hu.hm.fitjourneyapi.model.User;
-import hu.hm.fitjourneyapi.model.fitness.Workout;
-import org.mapstruct.factory.Mappers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
-import java.time.LocalDate;
-import java.util.UUID;
 
 @SpringBootApplication
 @EnableJpaAuditing
