6a3dca00b9833083796370943d32cb5bcafb13f6
Herczeg Martinez <herczegmartinez@gmail.com>
2026-01-11 18:42:25 +0100

    feat: working websocket

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/config/WebSocketConfig.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/config/WebSocketConfig.java
@@ -12,12 +12,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("http://localhost:5173");
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
+
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOrigins("http://localhost:5173");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue");
+        config.enableSimpleBroker("/topic", "/queue", "/user");
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
