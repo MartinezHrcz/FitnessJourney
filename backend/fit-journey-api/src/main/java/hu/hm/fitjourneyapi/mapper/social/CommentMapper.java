f92c8b9c3488a32180d9081c1505321a7753aab8
Herczeg Martinez <herczegmartinez@gmail.com>
2026-01-25 19:18:36 +0100

    feat: complete comment service and comment mapper, add empty comment controller

--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/CommentMapper.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/mapper/social/CommentMapper.java
@@ -1,13 +1,16 @@
 package hu.hm.fitjourneyapi.mapper.social;
 
 import hu.hm.fitjourneyapi.dto.social.comment.CommentDTO;
+import hu.hm.fitjourneyapi.model.User;
 import hu.hm.fitjourneyapi.model.social.Comment;
+import hu.hm.fitjourneyapi.model.social.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
+import java.util.UUID;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CommentMapper {
@@ -20,6 +23,8 @@ public interface CommentMapper {
     @Mapping(source = "user.username", target = "userName")
     @Mapping(source = "user.profileImageUrl", target = "userProfilePicture")
     CommentDTO toCommentDTO(Comment comment);
+
+    Comment toComment(String content, Post post, User user);
 
     List<CommentDTO> toCommentDTOs(List<Comment> comments);
 }
--- backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/implementation/social/CommentServiceImpl.java
+++ backend/fit-journey-api/src/main/java/hu/hm/fitjourneyapi/services/implementation/social/CommentServiceImpl.java
@@ -4,8 +4,12 @@ import hu.hm.fitjourneyapi.dto.social.comment.CommentCreateDTO;
 import hu.hm.fitjourneyapi.dto.social.comment.CommentDTO;
 import hu.hm.fitjourneyapi.dto.social.message.CreateMessageDTO;
 import hu.hm.fitjourneyapi.mapper.social.CommentMapper;
+import hu.hm.fitjourneyapi.model.User;
 import hu.hm.fitjourneyapi.model.social.Comment;
+import hu.hm.fitjourneyapi.model.social.Post;
+import hu.hm.fitjourneyapi.repository.UserRepository;
 import hu.hm.fitjourneyapi.repository.social.CommentRepository;
+import hu.hm.fitjourneyapi.repository.social.PostRepository;
 import hu.hm.fitjourneyapi.services.interfaces.social.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,10 +23,14 @@ import java.util.UUID;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
     private final CommentMapper mapper;
 
-    public CommentServiceImpl(CommentRepository commentRepository, CommentMapper mapper) {
+    public CommentServiceImpl(CommentRepository commentRepository, UserRepository userRepository, PostRepository postRepository, CommentMapper mapper) {
         this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
         this.mapper = mapper;
     }
 
@@ -67,16 +75,50 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public CommentDTO createComment(CommentCreateDTO commentDTO, UUID postId, UUID userId) {
-        return null;
+        log.debug("Creating new comment for post {}", postId);
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> {
+                    log.warn("Post with id {} not found", postId);
+                    return new NoSuchElementException("Post with id "+ postId+" not found");
+                }
+        );
+
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> {
+                    log.warn("Post with id {} not found", postId);
+                    return new NoSuchElementException("Post with id "+ postId+" not found");
+                }
+        );
+
+        Comment comment = mapper.toComment(commentDTO.getContent(), post, user);
+
+        log.info("Created comment with id {} ", comment.getId());
+        return  mapper.toCommentDTO(commentRepository.save(comment));
     }
 
     @Override
     public CommentDTO updateComment(UUID id, CreateMessageDTO commentDTO) {
-        return null;
+        Comment comment = commentRepository.findById(id).orElseThrow(
+                () -> {
+                    log.warn("Comment with id {} not found", id);
+                    return new NoSuchElementException("Comment with id "+ id +" not found");
+                }
+        );
+
+        comment.setContent(commentDTO.getContent());
+
+        comment = commentRepository.save(comment);
+
+        return mapper.toCommentDTO(comment);
     }
 
     @Override
     public void deleteComment(UUID id) {
-
+        log.debug("Deleting comment with id {}", id);
+        commentRepository.deleteById(id);
+        if (commentRepository.existsById(id))
+        {
+            throw new IllegalStateException("Comment with id " + id + " was not deleted");
+        }
     }
 }
